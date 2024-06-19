b3d918d4cb8035cf95af21cbc4a8cca1257c4db2
Mohammed Ahmed <46822007+mu7ammed-a7med@users.noreply.github.com>
2024-06-19 22:52:44 +0300

    Add files via upload

---
+++ 01 Python/Tasks/Task1c.py
@@ -0,0 +1,12 @@
+import os
+from pprint import pprint
+
+# Access all environment variables
+pprint(dict(os.environ), width = 1)
+
+# Access a particular enivronment variable
+print('\n', os.environ['HOME'], '\n')
+
+pprint(os.environ['PATH'])
+
+
---
+++ 01 Python/Tasks/Task2.py
@@ -0,0 +1,2 @@
+radius = float(input("Enter radius: "))
+print("Area of a Circle: %0.3f" %((radius**2) * 3.14159))
