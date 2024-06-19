c2f4dae419c5c3a325726e34982ea4a0987be2f5
Mohammed Ahmed <46822007+mu7ammed-a7med@users.noreply.github.com>
2024-06-19 22:57:53 +0300

    Delete 01 Python/Task8.py

--- 01 Python/Task8.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-print(f"This is the name/path of hte script {sys.argv[0]}")
-
-print(f"Number of arguments: {len(sys.argv)}")
-print("Argument list: ", end = '')
-for i in sys.argv:
-    print(i, end = ' ')
