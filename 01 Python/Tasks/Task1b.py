26d6e77241889ac7e8e1a1c87a1c4402063a197f
Mohammed Ahmed <46822007+mu7ammed-a7med@users.noreply.github.com>
2024-06-27 01:01:04 +0300

    Delete 01 Python/Tasks/Task1b.py

--- 01 Python/Tasks/Task1b.py
+++
@@ -1,7 +0,0 @@
-def isVowel(char):
-    char = char.lower()
-    vowels = "aeiou"
-    print("vowel") if char in vowels else print("Not a Vowel")
-
-letter = input("Enter a letter: ")
-isVowel(letter)
